5bf7c15b8dc65c2f8d989665ce4e278bbce84ee4
= <=>
2018-06-05 16:04:14 -0400

    First commit

---
+++ app/src/main/java/io/nanoapp/rockzom/garageace/landingPage.java
@@ -0,0 +1,13 @@
+package io.nanoapp.rockzom.garageace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class landingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
